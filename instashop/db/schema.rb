ee95dd149aaebfc0f47bde7cb4f2f9679a640e22
pipah00 <afifahdotcom92@gmail.com>
2015-07-08 14:51:29 +0000

    Changed colours

--- instashop/db/schema.rb
+++ instashop/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150705010649) do
+ActiveRecord::Schema.define(version: 20150708144400) do
 
   create_table "inventories", force: :cascade do |t|
     t.integer  "product_id"
