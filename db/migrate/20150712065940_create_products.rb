18092c096999500f021a1e1e67b7a8585fecad3e
pipah00 <afifahdotcom92@gmail.com>
2015-08-11 17:20:32 +0000

    Added order.rb model

--- db/migrate/20150712065940_create_products.rb
+++ db/migrate/20150712065940_create_products.rb
@@ -1,11 +1,15 @@
 class CreateProducts < ActiveRecord::Migration
   def change
-    create_table :products do |t|
-      t.string :name
-      t.decimal :price
-      t.text :description
-      t.integer :quantity
-      t.boolean :shipping
+    create_table :orders do |t|
+      t.integer :product_id
+      t.string :product_name
+      t.integer :selling_price
+      t.integer :quantity_sold
+      t.string :customer_name
+      t.string :customer_add
+      t.integer :customer_contact
+      t.string :customer_email
+      t.string :customer_payment
 
       t.timestamps null: false
     end
